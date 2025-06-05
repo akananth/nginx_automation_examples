89ea33eb5740ff054ba63466f6578c434b344709
Akash <60446741+akananth@users.noreply.github.com>
2025-06-05 18:52:54 -0500

    Update output.tf

--- azure/nginxaas/output.tf
+++ azure/nginxaas/output.tf
@@ -1,14 +1,16 @@
-# outputs.tf
 output "resource_group_name" {
-  value = azurerm_resource_group.main.name
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.main.name
 }
 
 output "subnet_id" {
-  value = azurerm_subnet.main.id
+  description = "ID of the created subnet"
+  value       = azurerm_subnet.main.id
 }
 
 output "managed_identity_id" {
-  value = azurerm_user_assigned_identity.main.id
+  description = "ID of the user-assigned managed identity"
+  value       = azurerm_user_assigned_identity.main.id
 }
 
 output "nginx_endpoint" {
@@ -17,21 +19,37 @@ output "nginx_endpoint" {
 }
 
 output "nginx_deployment_id" {
-  description = "NGINXaaS deployment ID"
+  description = "ID of the NGINXaaS deployment"
   value       = azurerm_nginx_deployment.main.id
 }
 
 output "vm_public_ips" {
-  description = "Public IP addresses of the VMs"
+  description = "Public IP addresses of the created VMs"
   value       = azurerm_public_ip.vm_pip[*].ip_address
 }
 
 output "vm_ssh_commands" {
-  description = "SSH access commands for the VMs"
+  description = "SSH commands to connect to the created VMs"
   value       = [for ip in azurerm_public_ip.vm_pip : "ssh adminuser@${ip.ip_address}"]
 }
 
 output "vm_ids" {
-  description = "IDs of the created VMs"
+  description = "IDs of the Azure Linux virtual machines"
   value       = [for vm in azurerm_linux_virtual_machine.nginx_vm : vm.id]
 }
+
+output "grafana_name" {
+  value       = azurerm_dashboard_grafana.this.name
+  description = "The name of the Azure Managed Grafana resource"
+}
+
+output "grafana_url" {
+  value       = azurerm_dashboard_grafana.this.endpoint
+  description = "The public endpoint of the Grafana dashboard"
+}
+
+output "log_analytics_workspace_name" {
+  description = "Name of the Log Analytics Workspace"
+  value       = azurerm_log_analytics_workspace.nginx_logging.name
+}
+
