dc57e01b8dc8c3fdf4a55752a07ec3ed2c1e32fb
Akash <akashdan90@gmail.com>
2025-06-05 20:10:09 -0500

    apply

--- azure/nginxaas/azurelog.tf
+++ azure/nginxaas/azurelog.tf
@@ -28,5 +28,5 @@ resource "azurerm_monitor_diagnostic_setting" "nginx_diagnostics" {
     }
   }
 
-  depends_on = [azurerm_nginx_configuration]
+  depends_on = [azurerm_nginx_configuration.main]
 }
