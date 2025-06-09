8d981a0fa0655c540606c5e854f516066a03bc5b
Akash <akashdan90@gmail.com>
2025-06-09 16:36:42 -0500

    Azure apply

--- azure/nginxaas/grafana.tf
+++ azure/nginxaas/grafana.tf
@@ -20,3 +20,45 @@ resource "azurerm_role_assignment" "grafana_viewer" {
 
   depends_on = [azurerm_dashboard_grafana.this]
 }
+
+resource "azurerm_template_deployment" "grafana_dashboard_import" {
+  name                = "grafana-n4-dashboard-import"
+  resource_group_name = azurerm_resource_group.main.name
+  deployment_mode     = "Incremental"
+
+  parameters = {
+    grafanaName     = azurerm_dashboard_grafana.this.name
+    dashboardName   = "n4-dashboard"
+    dashboardJson   = base64encode(file("${path.module}/nginxaas/n4-dashboard.json"))
+  }
+
+  template_body = <<DEPLOY
+{
+  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "grafanaName": {
+      "type": "string"
+    },
+    "dashboardName": {
+      "type": "string"
+    },
+    "dashboardJson": {
+      "type": "string"
+    }
+  },
+  "resources": [
+    {
+      "type": "Microsoft.Dashboard/grafana/dashboards",
+      "apiVersion": "2022-08-01",
+      "name": "[concat(parameters('grafanaName'), '/', parameters('dashboardName'))]",
+      "properties": {
+        "dashboard": "[base64ToString(parameters('dashboardJson'))]"
+      }
+    }
+  ]
+}
+DEPLOY
+
+  depends_on = [azurerm_dashboard_grafana.this]
+}
