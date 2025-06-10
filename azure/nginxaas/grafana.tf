5f5f5e7babf0a44c3339d61d1a9184a502550b35
Akash <akashdan90@gmail.com>
2025-06-09 22:40:48 -0500

    Azure apply

--- azure/nginxaas/grafana.tf
+++ azure/nginxaas/grafana.tf
@@ -1,7 +1,13 @@
+# Data source to dynamically get the Role Definition ID for "Grafana Viewer"
+data "azurerm_role_definition" "grafana_viewer" {
+  name  = "Grafana Viewer"
+  scope = "/subscriptions/${var.subscription_id}"
+}
+
 resource "azurerm_dashboard_grafana" "this" {
-  name                   = "${var.project_prefix}-grafana"
-  location               = azurerm_resource_group.main.location
-  resource_group_name    = azurerm_resource_group.main.name
+  name                = "${var.project_prefix}-grafana"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   identity {
     type = "SystemAssigned"
@@ -14,9 +20,9 @@ resource "azurerm_dashboard_grafana" "this" {
 }
 
 resource "azurerm_role_assignment" "grafana_viewer" {
-  scope                = azurerm_dashboard_grafana.this.id     # use "this" here
-  role_definition_name = "Grafana Viewer"
-  principal_id         = azurerm_user_assigned_identity.main.principal_id
+  scope              = azurerm_dashboard_grafana.this.id
+  role_definition_id = data.azurerm_role_definition.grafana_viewer.id
+  principal_id       = azurerm_user_assigned_identity.main.principal_id
 
   depends_on = [
     azurerm_dashboard_grafana.this
