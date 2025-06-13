dd115e9965d4fb6cd7b17dfcb34f16b1f6b42dde
Akash <akashdan90@gmail.com>
2025-06-13 11:06:38 -0500

    new apply

--- azure/nginxaas/grafana.tf
+++ azure/nginxaas/grafana.tf
@@ -1,4 +1,4 @@
-# Get current subscription (needed for scoping role assignments)
+# Get current subscription (needed for role scopes)
 data "azurerm_subscription" "current" {}
 
 # Get role definition for Grafana Admin
@@ -13,7 +13,7 @@ data "azurerm_role_definition" "monitoring_reader" {
   scope = data.azurerm_subscription.current.id
 }
 
-# Create Grafana instance
+# Create Grafana instance with system-assigned identity
 resource "azurerm_dashboard_grafana" "grafana" {
   name                = "${var.project_prefix}-grafana"
   location            = azurerm_resource_group.main.location
@@ -27,19 +27,7 @@ resource "azurerm_dashboard_grafana" "grafana" {
   grafana_major_version = "11"
 }
 
-# Get the Grafana Azure AD Application automatically (must be after grafana creation)
-data "azuread_application" "grafana_app" {
-  display_name = azurerm_dashboard_grafana.grafana.name
-  depends_on   = [azurerm_dashboard_grafana.grafana]
-}
-
-# Get the Service Principal associated with the Grafana application
-data "azuread_service_principal" "grafana_sp" {
-  application_id = data.azuread_application.grafana_app.application_id
-  depends_on     = [data.azuread_application.grafana_app]
-}
-
-# Assign Grafana Admin role to each provided user
+# Assign Grafana Admin role to each provided user (Object IDs)
 resource "azurerm_role_assignment" "grafana_admin_users" {
   for_each = toset(var.grafana_admin_object_ids)
 
@@ -59,39 +47,20 @@ resource "azurerm_role_assignment" "grafana_admin_app" {
   depends_on = [azurerm_dashboard_grafana.grafana]
 }
 
-# Comprehensive monitoring permissions
+# Assign Monitoring Reader role to Grafana identity for subscription, RG, and NGINX resource
 resource "azurerm_role_assignment" "grafana_monitoring_permissions" {
   for_each = {
-    # Cover all necessary scopes
-    "subscription" = data.azurerm_subscription.current.id,
-    "resource_group" = azurerm_resource_group.main.id,
-    "nginx" = azurerm_nginx_deployment.main.id
+    subscription   = data.azurerm_subscription.current.id,
+    resource_group = azurerm_resource_group.main.id,
+    nginx          = azurerm_nginx_deployment.main.id
   }
 
   scope              = each.value
   role_definition_id = data.azurerm_role_definition.monitoring_reader.id
-  # Assign to both identities
   principal_id       = azurerm_dashboard_grafana.grafana.identity[0].principal_id
 
   depends_on = [
     azurerm_dashboard_grafana.grafana,
     azurerm_nginx_deployment.main
-  ]
-}
-
-# Additional assignment for the Service Principal if needed
-resource "azurerm_role_assignment" "grafana_sp_monitoring_permissions" {
-  for_each = {
-    "subscription" = data.azurerm_subscription.current.id,
-    "resource_group" = azurerm_resource_group.main.id
-  }
-
-  scope              = each.value
-  role_definition_id = data.azurerm_role_definition.monitoring_reader.id
-  principal_id       = data.azuread_service_principal.grafana_sp.object_id
-
-  depends_on = [
-    data.azuread_service_principal.grafana_sp,
-    azurerm_dashboard_grafana.grafana
   ]
 }
